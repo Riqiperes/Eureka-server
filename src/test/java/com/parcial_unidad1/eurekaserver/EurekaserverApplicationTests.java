6b0687f005d625a4baa03a1df91470a7eab1cd02
Riqiperes <riqiperes14@gmail.com>
2026-03-11 16:15:33 -0600

    Examen Parcial: Implementación de microservicios e infraestructura

---
+++ src/test/java/com/parcial_unidad1/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parcial_unidad1.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
